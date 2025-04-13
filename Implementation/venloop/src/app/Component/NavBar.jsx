9362170046720a23678e81ed4530d8a2277e8c9f
felixgoertz <f.gortz@student.fontys.nl>
2025-04-13 12:58:22 +0200

    ?

---
+++ Implementation/venloop/src/app/Component/NavBar.jsx
@@ -0,0 +1,11 @@
+
+
+export default function NavBar() {
+    return (
+        <header className="bg-[#1F2A60] shadow-md px-6 py-4 flex justify-between items-center z-30 relative">
+            <div />
+            <img src="/Avond4daagse_diapositief.png" alt="Logo" className="h-8" />
+            <span className="text-white-700 hover:text-blue-600 cursor-pointer">Account</span>
+        </header>
+    );
+}
---
+++ Implementation/venloop/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import TeamForm from "@/app/Component/TeamForm";
+import AdminLandingPage from "@/app/AdminLandingPage";
+
+export default function Home() {
+  return (
+  <AdminLandingPage />
+  );
+}
