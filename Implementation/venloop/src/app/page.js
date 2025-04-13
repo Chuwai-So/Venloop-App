e9ab3908182225c991edef55f3437aae5a65498c
Chuwai-So <c.so@student.fontys.nl>
2025-04-13 13:07:03 +0200

    hosting done

--- Implementation/venloop/src/app/page.js
+++ Implementation/venloop/src/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import TeamForm from "@/app/Component/TeamForm";
+import Penis from "@/app/Component/Penis"
 
 export default function Home() {
   return (
-  <TeamForm />
+     <Penis />
   );
 }
+``
