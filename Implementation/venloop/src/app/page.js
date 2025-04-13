96c91e568fbc54d10c570c2d87ab6494c65ec359
felixgoertz <f.gortz@student.fontys.nl>
2025-04-13 12:57:54 +0200

    ?

--- Implementation/venloop/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import TeamForm from "@/app/Component/TeamForm";
-import AdminLandingPage from "@/app/AdminLandingPage";
-
-export default function Home() {
-  return (
-  <AdminLandingPage />
-  );
-}
