e0b0c0948d100b26a43dcf355bf86a09551349b6
Chuwai-So <c.so@student.fontys.nl>
2025-04-25 11:04:19 +0200

    fix landing html

--- Implementation/venloop/src/app/page.js
+++ Implementation/venloop/src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import TeamForm from "@/app/Component/TeamForm";
 import Penis from "@/app/Component/Penis"
+import LoginPage from "@/app/admin-login/page";
+import AdminLandingPage from "@/app/admin-landing/page";
 
 export default function Home() {
   return (
-     <Penis />
+     <AdminLandingPage />
   );
 }
-``
